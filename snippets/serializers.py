4e100c3fd42068bbdc6fb8928067613fed671997
ZujkisNx <pauliusruzgas@gmail.com>
2014-06-09 23:37:27 +0300

    Added view for listing and creating snippets.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,6 +4,10 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.Serializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
     pk = serializers.Field()  # Note: `Field` is an untyped read-only field.
     title = serializers.CharField(required=False,
                                   max_length=100)
